ea82142cf1e36b1cbd89f6d4eabef0486f2e4807
Saske912 <saveloy@yandex.ru>
2022-11-14 23:51:32 +0300

    gh actions

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ export default function Home({ allPostsData }) {
         <section className={utilStyles.headingMd}>
           <p>[Your Self Introduction]</p>
           <p>
+              test actions
             (This is a sample website - you’ll be building a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
           </p>
